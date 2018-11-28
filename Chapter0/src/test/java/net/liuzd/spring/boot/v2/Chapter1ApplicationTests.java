9d481a23d71bd7b39dd9313e58a874469a4a6a3e
liuzidong <liuzidong@jd.com>
2018-11-28 15:02:16 +0800

    Spring Boot 2 第一个示例

---
+++ Chapter0/src/test/java/net/liuzd/spring/boot/v2/Chapter1ApplicationTests.java
@@ -0,0 +1,39 @@
+package net.liuzd.spring.boot.v2;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import net.liuzd.spring.boot.v2.web.HelloController;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class Chapter1ApplicationTests {
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
+	}
+
+}
