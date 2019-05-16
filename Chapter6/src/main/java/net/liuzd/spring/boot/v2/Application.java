28799f6449d17472fae07d526d021e189bec5db3
liuzidong <liuzidong@jd.com>
2019-05-16 17:06:56 +0800

    FIX WebSocket

--- Chapter6/src/main/java/net/liuzd/spring/boot/v2/Application.java
+++ Chapter6/src/main/java/net/liuzd/spring/boot/v2/Application.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	  @Bean
-	    public TomcatServletWebServerFactory servletContainer(){
-	        return new TomcatServletWebServerFactory(80) ;
-	    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        return new TomcatServletWebServerFactory(80);
+    }
 
 }
